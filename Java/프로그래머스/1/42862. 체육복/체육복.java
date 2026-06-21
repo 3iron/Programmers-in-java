3a5c3e15748a2c3457e62baba7f62943b48a703e
3iron <82188346+3iron@users.noreply.github.com>
2026-06-21 15:16:05 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int[] clothes = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            clothes[i] = 1;
+        }
+
+        for (int l : lost) {
+            clothes[l]--;
+        }
+
+        for (int r : reserve) {
+            clothes[r]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (clothes[i] == 2) {
+                if (i > 1 && clothes[i - 1] == 0) {
+                    clothes[i]--;
+                    clothes[i - 1]++;
+                } else if (i < n && clothes[i + 1] == 0) {
+                    clothes[i]--;
+                    clothes[i + 1]++;
+                }
+            }
+        }
+
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (clothes[i] >= 1) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
